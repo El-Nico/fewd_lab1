9278fdd2daa5852d66272d4d160039ea7ec0cf63
El-Nico <nicholasc1665@yahoo.com>
2020-01-28 15:30:07 +0000

    lab 1 final

---
+++ app.js
@@ -0,0 +1,15 @@
+var firstName="Nicholas";
+var favouriteColour="blue";
+var favouriteFood="rice";
+var city="Dublin";
+
+//prints name color and food to the console
+console.log(firstName);
+console.log(favouriteColour);
+console.log(favouriteFood);
+
+var phrase='A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all. '
+, endingPhrase=' is what we see. Rainbow colours, are made with light bending you see!', 
+one='red', two='orange', three='yellow', four='green', five='blue', six='indigo', seven='violet';
+
+console.log(phrase + one +', '+ two +', '+ three +', '+ four +', '+ five +', '+ six +', '+ seven +endingPhrase);
